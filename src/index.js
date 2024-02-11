f28c93872ee27571a60c9bc26aa0a14b2bccbd5a
suela <>
2024-02-11 21:43:17 +0100

    Implemented Score Scene

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import Phaser from "phaser";
 import PlayScene from "./scenes/PlayScene";
 import MenuScene from "./scenes/MenuScene";
 import PreloadScene from "./scenes/PreloadScene";
+import ScoreScene from "./scenes/ScoreScene";
 
 const WIDTH = 800;
 const HEIGHT = 600;
@@ -12,7 +13,7 @@ const SHARED_CONFIG = {
     height: HEIGHT,
     startPosition: BIRD_POSITION
 }
-const Scenes = [PreloadScene, MenuScene, PlayScene];
+const Scenes = [PreloadScene, MenuScene, ScoreScene, PlayScene];
 const createScene = Scene => new Scene(SHARED_CONFIG);
 const initScenes = () => Scenes.map(createScene);
 
--- src/scenes/PreloadScene.js
+++ src/scenes/PreloadScene.js
@@ -7,7 +7,8 @@ class PreloadScene extends Phaser.Scene {
         this.load.image('sky', 'assets/sky.png');
         this.load.image('bird', 'assets/bird.png');
         this.load.image('pipe', 'assets/pipe.png');
-        this.load.image('pause', 'assets/pause.png')
+        this.load.image('pause', 'assets/pause.png');
+        this.load.image('back', 'assets/back.png');
     }
 
     create() {
