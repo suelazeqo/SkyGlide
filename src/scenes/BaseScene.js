dfc8d1bdc791a0d57cbc617d6ef5b4ff15cf9c0f
suela <>
2024-02-11 18:49:41 +0100

    Created BaseScene, MenuScene and PreloadScene

---
+++ src/scenes/BaseScene.js
@@ -0,0 +1,11 @@
+class BaseScene extends Phaser.Scene{
+    constructor(key, config) {
+        super(key);
+        this.config = config;
+    }
+
+    create(){
+        this.add.image(0, 0, 'sky').setOrigin(0);
+    }
+}
+export default BaseScene
