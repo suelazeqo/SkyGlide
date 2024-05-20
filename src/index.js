36816db86a631e6641f569c34e0593904015522a
suelazeqo <suelazeqo19@gmail.com>
2024-05-20 13:44:37 +0200

    Created Bird Spritesheet and implemented animations

--- src/index.js
+++ src/index.js
@@ -21,11 +21,9 @@ const initScenes = () => Scenes.map(createScene);
 const config = {
     type: Phaser.AUTO,
     ...SHARED_CONFIG,
+    pixelArt:true,
     physics: {
         default: 'arcade',
-        arcade: {
-            debug: true
-        }
     },
     scene: initScenes()
 }
